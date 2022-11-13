dd7b14145059f76cb53a898b266f8ebf82f98627
Juan <jpcasas@gmail.com>
2022-11-13 10:01:37 -0500

    all in one mehotd :(

--- src/main/java/dev/jpcasas/pubsub/Hub2mqtt.java
+++ src/main/java/dev/jpcasas/pubsub/Hub2mqtt.java
@@ -11,6 +11,7 @@ import javax.transaction.Transactional;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import dev.jpcasas.clients.HubBridgeSubscriber;
+import dev.jpcasas.configuration.HubInitialConfiguration;
 import dev.jpcasas.model.Configuration;
 import dev.jpcasas.model.SettingsEnum;
 import dev.jpcasas.repositories.ConfigurationRepo;
@@ -27,39 +28,43 @@ public class Hub2mqtt {
     @Inject
     ConfigurationRepo crepo;
 
+    @Inject
+    HubInitialConfiguration initc;
+
     @Outgoing("hubevents")
     @Broadcast
     @ActivateRequestContext
     public Multi<String> generate() {
-
-        Optional<Configuration> hubip = crepo.findById(SettingsEnum.HUB_IP.getKey());
-        Optional<Configuration> appId = crepo.findById(SettingsEnum.APP_ID.getKey());
-
         log.info("HUB OUT");
-        if (hubip.isPresent() && appId.isPresent()) {
+       
+        while (!initc.isCongifured()) {
+            initc.registerApplication();
 
-            return Multi.createFrom().emitter(consumer -> {
-                try {
-                    log.info("creating emitter");
-                    
-                    subscriber.getEventSource(hubip.get().getConfigValue(), appId.get().getConfigValue()).register(ie -> {
-                        log.info("Sending events");
-                        String data = ie.readData();
-                        log.info(data);
-                        consumer.emit(data);
-
-                    }, error -> {
-                        error.printStackTrace();
-                    }, () -> {
-                        log.info("END");
-                    });
-                } catch (KeyManagementException | NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                }
-
-            });
-        }else{
-            return Multi.createFrom().items("NO HUB FOUND OR NOT HUB's BUTTON PUSHED");
         }
+        
+        String hubip = initc.getHubIp();
+        String appId = initc.getAppId();
+
+        // Don't wan to verify if it's null -> should not
+        return Multi.createFrom().emitter(consumer -> {
+            try {
+                log.info("creating emitter");
+                subscriber.getEventSource(hubip, appId).register(ie -> {
+                    log.info("Sending events");
+                    String data = ie.readData();
+                    log.info(data);
+                    consumer.emit(data);
+
+                }, error -> {
+                    error.printStackTrace();
+                }, () -> {
+                    log.info("END");
+                });
+            } catch (KeyManagementException | NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+
+        });
+
     }
 }
